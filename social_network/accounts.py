3e2bfaf2696ba38518cd2cc0fb998ed37889d3cb
pcrrtt3 <62735017+pcrrtt3@users.noreply.github.com>
2020-05-31 22:09:25 -0700

    Social Network Solution

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,29 @@
-
 class User(object):
+    def __repr__(self):
+        return '[<User: "{} {}">]'.format(self.first_name, self.last_name)
+    
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        if hasattr(self, 'posts'):
+            self.posts.append(post)
+        else:
+            self.posts=[]
+            self.posts.append(post)
+    
 
-    def get_timeline(self):
-        pass
+    def get_timeline(self):    
+        timeline_list=[n for x in self.following if hasattr(x, 'posts')==True for n in x.posts]
+        timeline_list.sort(key=lambda x: x.timestamp, reverse=True)
+        return timeline_list
 
     def follow(self, other):
-        pass
+        if hasattr(self, 'following'):
+            self.following.append(other)
+        else:
+            self.following=[]
+            self.following.append(other)
--- social_network/posts.py
+++ social_network/posts.py
@@ -4,33 +4,69 @@ from datetime import datetime
 # create these classes as it corresponds:
 # (your tests will fail if you don't comment out these classes)
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text=text
+        self.user=None
+        if timestamp==None:
+            self.timestamp=datetime.now()        
+        else:
+            self.timestamp=timestamp
+            
+
+    def __repr__(self):
+        return '<{}: {}>'.format(self.__class__.__name__, self.text)
+    
+    def set_user(self, user):
+        self.user=user
+
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        super().__init__(text, timestamp)
+
+    def __str__(self):
+        first=getattr(self.user, 'first_name')
+        last=getattr(self.user, 'last_name')
+        txt=getattr(self, 'text')
+        if self.timestamp!=None:
+            date_post=getattr(self, 'timestamp').strftime("%A, %b %d, %Y")
+            return '@{} {}: "{}"\n\t{}'.format(first, last, txt, date_post)
+        else:
+            return '@{} {}: "{}"'.format(first, last, txt)
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        super().__init__(text, timestamp)
+        self.image_url=image_url
+    
+    def __str__(self):
+        first=getattr(self.user, 'first_name')
+        last=getattr(self.user, 'last_name')
+        txt=getattr(self, 'text')
+        image_u=getattr(self, 'image_url')
+        if self.timestamp!=None:
+            date_post=getattr(self, 'timestamp').strftime("%A, %b %d, %Y")
+            return '@{} {}: "{}"\n\t{}\n\t{}'.format(first, last, txt, image_u, date_post)
+        else:
+            return '@{} {}: "{}"\n\t{}'.format(first, last, txt, image_u)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
-
-
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super().__init__(text, timestamp)
+        self.latitude=latitude
+        self.longitude=longitude
+    
+    def __str__(self):
+        first=getattr(self.user, 'first_name')
+        last=getattr(self.user, 'last_name')
+        txt=getattr(self, 'text')
+        lat=getattr(self, 'latitude')
+        longg=getattr(self, 'longitude')
+        if self.timestamp!=None:
+            date_post=getattr(self, 'timestamp').strftime("%A, %b %d, %Y")
+            return '@{} Checked In: "{}"\n\t{}, {}\n\t{}'.format(first, txt, lat, longg, date_post)
+        else:    
+            return '@{} Checked In: "{}"\n\t{}, {}'.format(first, txt, lat, longg)
